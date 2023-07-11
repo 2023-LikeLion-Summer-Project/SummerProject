4abfd037194d1875db34a3b65f44e93c24436ce1
evaaannnnn <vmfhrmfoald36@gmail.cmom>
2023-07-11 21:07:33 +0900

    create frontend router file

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,13 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Main from "./routes/main";
 
-function App() {
-  return <div>hi</div>;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path={"/main"} element={<Main />} />
+    </Routes>
+  );
+};
 
 export default App;
---
+++ src/main/frontend/src/routes/main.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import logo from "./img/logo.png";
+
+function main() {
+  return (
+    <div className="container">
+      <div className="mainbar">
+        <div className="logo"></div>
+        <div className="text"></div>
+        <div className="profile"></div>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default main;
