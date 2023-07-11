bd06cdd4c7c428a31d6d4a1143b55f4b8c6e9422
evaaannnnn <vmfhrmfoald36@gmail.cmom>
2023-07-11 21:11:51 +0900

    create frontend

--- src/main/frontend/src/routes/main.js
+++ src/main/frontend/src/routes/main.js
@@ -1,11 +1,14 @@
 import React from "react";
-// import logo from "./img/logo.png";
+import logo from "./img/logo.png";
 
 function main() {
   return (
     <div className="container">
       <div className="mainbar">
-        <div className="logo"></div>
+        <div className="logo">
+          <div>
+          </div>
+        </div>
         <div className="text"></div>
         <div className="profile"></div>
       </div>
