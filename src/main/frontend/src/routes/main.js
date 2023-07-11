f87e213e2a36d9399ee26b1aa9665b8a894799a4
evaaannnnn <vmfhrmfoald36@gmail.cmom>
2023-07-11 21:16:22 +0900

    test

---
+++ src/main/frontend/src/routes/main.js
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "./img/logo.png";
+
+function main() {
+  return (
+    <div className="container">
+      <div className="mainbar">
+        <div className="logo">
+          <div>
+            <img src={logo} />
+          </div>
+        </div>
+        <div className="text"></div>
+        <div className="profile"></div>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default main;
