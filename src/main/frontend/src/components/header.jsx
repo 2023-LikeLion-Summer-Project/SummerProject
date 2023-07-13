9968f2fb3b6801570b2d4f4caab13eab23406096
zoey <yoonzoeym27@gmail.com>
2023-07-13 19:30:47 +0900

    [ysy]frontend

--- src/main/frontend/src/components/header.jsx
+++ src/main/frontend/src/components/header.jsx
@@ -1,21 +1,83 @@
 import React from "react";
-import "./header.css";
+import styled from "styled-components";
+import logo from "../img/spacepic2.png";
+import profile from "../img/profilepic.png";
+import { useNavigate } from "react-router-dom";
 
-const Header = () => {
+export default function Header({ ScrollActive }) {
+  const navigate = useNavigate();
+  const toArchive = () => {
+    navigate("/archive");
+  };
+
+  const toMain = () => {
+    navigate("/main");
+  };
+
   return (
-    <>
-      <header className="header">
-        <div className="contents">
-          <div className="name">logoipsum</div>
-          <div className="navigation">
-            <h4 className="item">Home</h4>
-            <h4 className="item">Archive</h4>
-            <h4 className="item">Profile</h4>
-          </div>
-        </div>
-      </header>
-    </>
+    <Positioner className={ScrollActive ? "flexible" : null}>
+      <HeaderContents>
+        <Logo src={logo}></Logo>
+        <Spacer />
+        <Button onClick={toMain}>Home</Button>
+        <Button onClick={toArchive}>Archive</Button>
+        <Profile src={profile}></Profile>
+      </HeaderContents>
+    </Positioner>
   );
-};
+}
 
-export default Header;
+// 상단 고정, 그림자
+const Positioner = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 35px;
+  // left: calc(50vw - 600px);
+  width: 100%;
+  padding: 0px;
+  border: 0px;
+
+  z-index: 99;
+
+  &.flexible {
+    position: fixed;
+  }
+`;
+
+const HeaderContents = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 1500px;
+  height: 100%;
+  margin: auto;
+  justify-content: space-between;
+`;
+
+const Logo = styled.img`
+  height: 30px;
+`;
+
+const Profile = styled.img`
+  margin-left: 20px;
+  margin-right: 20px;
+  height: 30px;
+  width: 30px;
+`;
+const Spacer = styled.div`
+  flex-grow: 1;
+`;
+
+const Button = styled.button`
+  margin-left: 20px;
+  margin-right: 20px;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 450;
+  line-height: normal;
+  letter-spacing: 0.2px;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  color: white;
+`;
--- src/main/frontend/src/routes/login.js
+++ src/main/frontend/src/routes/login.js
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 import "./login.css";
 import Header from "../components/header";
 import Layout from "../components/layout";
+import { useNavigate } from "react-router-dom";
 
-const LoginPage = () => {
+const LogIn = () => {
+  const REST_API_KEY = "27a229e3d0e076567b75011e231c7cda";
+  const REDIRECT_URI = "http://localhost:3000/main";
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  const KakaoLoginPage = () => {
+    window.location.href = KAKAO_AUTH_URL;
+  };
+
+  const navigate = useNavigate();
+  const GotoMain = () => {
+    navigate("/main");
+  };
+
+  const LostPw = () => {
+    navigate("/lostpw");
+  };
+
+  const [ScrollY, setScrollY] = useState(0);
+  const [ScrollActive, setScrollActive] = useState(false);
+
+  const handleScroll = () => {
+    if (ScrollY > 120) {
+      setScrollY(window.scrollY);
+      setScrollActive(true);
+    } else {
+      setScrollY(window.scrollY);
+      setScrollActive(false);
+    }
+  };
+
+  useEffect(() => {
+    function scrollListener() {
+      window.addEventListener("scroll", handleScroll);
+    } //  window 에서 스크롤을 감시 시작
+    scrollListener(); // window 에서 스크롤을 감시
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }; //  window 에서 스크롤을 감시를 종료
+  });
+
   return (
     <>
+      <Header ScrollActive={ScrollActive} />
       <Layout>
-        <div>
-          <Header />
-        </div>
         <div className="parent_div">
           <div className="main_name">
             <h1 className="ftext">logoipsum</h1>
@@ -23,12 +63,18 @@ const LoginPage = () => {
             <div className="inputbox">
               <input type="password" placeholder="비밀번호"></input>
             </div>
-            <button className="lostpwbtn">비밀번호 찾기</button>
+            <button className="lostpwbtn" onClick={LostPw}>
+              비밀번호 찾기
+            </button>
             <div className="loginbtns">
-              <button className="loginbtn">Login</button>
+              <button className="loginbtn" onClick={GotoMain}>
+                Login
+              </button>
             </div>
             <div className="loginbtns">
-              <button className="kakaobtn">카카오 로그인</button>
+              <button className="kakaobtn" onClick={KakaoLoginPage}>
+                카카오 로그인
+              </button>
             </div>
           </div>
         </div>
@@ -37,4 +83,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default LogIn;
